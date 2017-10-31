0d19a1dee49bdc0f24bcc3b62b2fa1b0490e8fd3
Anderson Njoroge <anderson21njoroge@gmail.com>
2017-10-31 11:37:50 +0300

    functions working in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,50 @@
+/*back-end*/
+function numberArray(hard){
+    var Numbers= [];
 
-  $(document).ready((){
- $("#blanks form").submit(function(){
-function closeWin() {
-  myWindow.close();
-  }
+    for (var a=1; a <=hard; a++){
+        if ((a % 3===0)&& (a%5===0)){
+            Numbers.push("PingPong");
+        }
+        else if (a%5===0){
+            rangeOfNumbers.push("Pong");
+
+        }
+        else if (a % 3===0){
+          Numbers.push("Ping");
+        }
+        else {
+            Numbers.push(a)
+        }
+    };
+    return Numbers;
+};
+/* back-end*/
+
+
+function clearForm(form) {
+        $(':input').not(':button, :submit').val('');
+    };
+
+/*Front-end*/
+$(document).ready(function(){
+$("form#pong").submit(function(event){
+event.preventDefault();
+
+$("ul").empty();
+
+var hard = parseInt($("#harder").val());
+
+var display= numberArray(harder);
+
+for (var index =0; index <= display.length; index++){
+
+
+$("ul#output").append("<li>"+display[index]+"</li>");
+     };
+     clearForm("#input");
+     });
+
 });
-});
+
+/*Front-end*/
